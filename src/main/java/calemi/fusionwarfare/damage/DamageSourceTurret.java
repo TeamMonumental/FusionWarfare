a363fa1bc5554d258f6a91aba1b38d57201353f3
Calemi <lemzsteamerson@gmail.com>
2015-07-16 19:41:26 -0400

    Turret work

---
+++ src/main/java/calemi/fusionwarfare/damage/DamageSourceTurret.java
@@ -0,0 +1,18 @@
+package calemi.fusionwarfare.damage;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ChatComponentTranslation;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.EntityDamageSourceIndirect;
+import net.minecraft.util.IChatComponent;
+import net.minecraft.util.StatCollector;
+
+public class DamageSourceTurret extends DamageSource {
+	
+	protected DamageSourceTurret() {
+		super("turret");
+	}
+}
--- src/main/java/calemi/fusionwarfare/tileentity/machine/TileEntityAuraTurret.java
+++ src/main/java/calemi/fusionwarfare/tileentity/machine/TileEntityAuraTurret.java
@@ -1,19 +1,71 @@
 package calemi.fusionwarfare.tileentity.machine;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.DamageSource;
 import calemi.fusionwarfare.block.BlockContainerBase;
+import calemi.fusionwarfare.damage.DamageSourceTurret;
 import calemi.fusionwarfare.tileentity.EnumIO;
 import calemi.fusionwarfare.tileentity.IEnergy;
 import calemi.fusionwarfare.tileentity.TileEntityBase;
 import calemi.fusionwarfare.tileentity.TileEntitySecurity;
+import calemi.fusionwarfare.util.EnergyUtil;
 
 public class TileEntityAuraTurret extends TileEntitySecurity {
 
 	public TileEntityAuraTurret() {
 		maxEnergy = 5000;
 		maxProgress = 100;
+	}
+	
+	@Override
+	public void updateEntity() {
+
+		int energyCost = 50;		
+		int range = 5;
+		
+		if (EnergyUtil.canSubtractEnergy(this, energyCost)) {
+			
+			progress++;
+			
+			if (isDone()) {
+		
+				resetProgress();
+				
+				for (Object player : worldObj.getEntitiesWithinAABB(EntityPlayer.class, AxisAlignedBB.getBoundingBox(xCoord, yCoord, zCoord, xCoord + 1, yCoord + 1, zCoord + 1).expand(range, range, range))) {
+				
+					if (!isSameTeam((EntityPlayer) player)) {
+											
+						((EntityPlayer)player).attackEntityFrom(new DamageSource("turret"), 2.0F);	
+						EnergyUtil.subtractEnergy(this, energyCost);
+					}
+				}
+			}
+		}
+	}
+	
+	//----------------------------------------------------------------------------
+	
+	@Override
+	public Packet getDescriptionPacket() {
+		NBTTagCompound syncData = new NBTTagCompound();
+	
+		writeToNBT(syncData);
+
+		return new S35PacketUpdateTileEntity(this.xCoord, this.yCoord, this.zCoord, 1, syncData);
+	}
+
+	@Override
+	public void onDataPacket(NetworkManager net, S35PacketUpdateTileEntity pkt) {
+
+		readFromNBT(pkt.func_148857_g());
 	}
 	
 	@Override
